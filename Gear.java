220649e235ca5851d0f20bc60d93da049663b511
ptripathi3 <47946476+ptripathi3@users.noreply.github.com>
2020-07-12 12:11:41 +0530

    Modified Gear class
    Method to calculate Gear Inches  introduced 
    A private method setWheel to set the value of wheel diameter
    Assuming values for rim and tire diameter if passed to the constructor are in inches. Also assuming a value for Cog and Chainring is always passed to the constructor.
    The code should be easy to change. Next task unknown

--- Gear.java
+++ Gear.java
@@ -1,5 +1,6 @@
 package pkg;
 
+
 public class Gear {
 	
 	//DATA MEMBERS
@@ -7,8 +8,22 @@ public class Gear {
 	private double chainring;		//No. of teeth in Chainring 
 	private double ratio;			//Ration of Chainring and Cog
 	private boolean setratio;		//bool value to know if ratio set or not
+	private double rim_dia;			//Rim diameter
+	private double tire_dia;		//Tire diameter
+	private double wheel_dia;		//Wheel diameter
+	private boolean setwheel;		//bool value to know if wheel diameter set or not
 	
 	//CONSTRUCTOR
+	public Gear(double chainring,int cog,double rim_dia, double tire_dia) {
+
+		this.chainring=chainring;
+		this.cog=cog;
+		this.rim_dia=rim_dia;
+		this.tire_dia=tire_dia;
+		this.setratio=false;
+		this.setwheel=false;
+	}	
+	
 	public Gear(double chainring,int cog) {
 
 		this.chainring=chainring;
@@ -25,8 +40,18 @@ public class Gear {
 	public double getChainring(){	//getter method for chainring
 		return chainring;
 	}
+	
+
 	public int getCog() {			//getter method for cog
 		return cog;
+	}
+	
+	public double getRim(){			//getter method for rim diameter
+		return rim_dia;
+	}
+	
+	public double getTire(){		//getter method for tire diameter
+		return tire_dia;
 	}
 	
 	public double getRatio() {		//getter method for ratio
@@ -36,7 +61,25 @@ public class Gear {
 		return ratio;
 		
 	}
-
+	
+	private void setWheel() {		//private method to set wheel diameter
+		wheel_dia=getRim()+(2*getTire());
+		setwheel=true;
+	}
+	
+	public double getWheel() {		//getter method for wheel diameter
+		if(setwheel==false)
+			setWheel();
+		return wheel_dia;
+		
+	}
+	
+	public double getGearInches() {			//getter method for gear inches
+		return getWheel()*getRatio();
+		
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
@@ -44,5 +87,10 @@ public class Gear {
 		Gear g2=new Gear(30,27);				//New object of class gear
 		System.out.println(g1.getRatio());		//print gear_inches for object g1
 		System.out.println(g2.getRatio());		//print gear_inches of object g2
+		
+		Gear g3=new Gear(52,11,26,1.5);				//New object of class gear
+		Gear g4=new Gear(52,11,24,1.25);			//New object of class gear
+		System.out.println(g3.getGearInches());		//print gear_inches for object g1
+		System.out.println(g4.getGearInches());		//print gear_inches of object g2
 	}
 }
